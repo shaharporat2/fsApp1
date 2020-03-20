9e6a1793cbace1cdbee45b94f91aaa8770b3b517
shahar <shaharporat2@gmail.com>
2020-03-20 17:07:31 +0200

    full REST API

--- 01-Server/business-logic-layer/vacations-logic.js
+++ 01-Server/business-logic-layer/vacations-logic.js
@@ -11,7 +11,7 @@ dal.connectAsync()
     .catch(err => console.log(err));
 
 /**
- * 
+ * Return all vacations from vacations collection
  */
 function getAllVacationsAsync() {
     return new Promise((resolve, reject) => {
@@ -24,7 +24,66 @@ function getAllVacationsAsync() {
         });
     });
 }
+/**
+ * 
+ * @param {id of the vacation} _id 
+ */
+function getOneVacationAsync(_id) {
+    return new Promise((resolve, reject) => {
+        Vacation.findOne({ _id }, (err, vacation) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(vacation);
+        });
+    });
+}
+
+/**
+ * 
+ * @param {a vacation object } vacation 
+ * update a vacation in the database
+ */
+function updateVacationtAsync(vacation) {
+    return new Promise((resolve, reject) => {
+        console.log(vacation._id)
+        Vacation.updateOne({ _id: vacation._id }, vacation, (err, info) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(info.n ? vacation : null); // info.n - מספר המוצרים שנמצאו
+        });
+    });
+}
+
+/**
+ * 
+ * @param {*} vacation 
+ * Insert vacation into the database
+ */
+function addVacationAsync(vacation) {
+    return vacation.save();
+}
+
+function deleteVacationAsync(_id) {
+    return new Promise((resolve, reject) => {
+        Vacation.deleteOne({ _id }, (err, info) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
 
 module.exports = {
-    getAllVacationsAsync
+    getAllVacationsAsync,
+    getOneVacationAsync,
+    updateVacationtAsync,
+    addVacationAsync,
+    deleteVacationAsync
 };
--- 01-Server/controllers/vacations-controller.js
+++ 01-Server/controllers/vacations-controller.js
@@ -15,4 +15,86 @@ router.get("/", async (request, response) => {
     }
 });
 
+// GET http://localhost:3000/api/products/7
+router.get("/:_id", async (request, response) => {
+    try {
+        const _id = request.params._id;
+        const vacation = await vacationsLogic.getOneVacationAsync(_id);
+
+        if (!vacation) {
+            response.sendStatus(404);
+            return;
+        }
+
+        response.json(vacation);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// PUT http://localhost:3000/api/vacations/7
+router.put("/:_id", async (request, response) => {
+    try {
+        const _id = request.params._id;
+        const vacation = new Vacation(request.body);
+        vacation._id = _id;
+        const updatedVacation = await vacationsLogic.updateVacationtAsync(vacation);
+
+        if (updatedVacation === null) {
+            response.sendStatus(404);
+            return;
+        }
+
+        response.json(updatedVacation);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// PATCH http://localhost:3000/api/vacations/7
+router.patch("/:_id", async (request, response) => {
+    try {
+        const _id = request.params._id;
+        const vacation = new Vacation(request.body);
+        vacation._id = _id;
+        const updatedVacation = await vacationsLogic.updateVacationtAsync(vacation);
+
+        if (updatedVacation === null) {
+            response.sendStatus(404);
+            return;
+        }
+
+        response.json(updatedVacation);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// POST http://localhost:3000/api/vacations
+router.post("/", async (request, response) => {
+    try {
+        const vacation = new Vacation(request.body);
+        const addedVacation = await vacationsLogic.addVacationAsync(vacation);
+        response.status(201).json(addedVacation);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
+// DELETE http://localhost:3000/api/products/7
+router.delete("/:_id", async (request, response) => {
+    try {
+        const _id = request.params._id;
+        await vacationsLogic.deleteVacationAsync(_id);
+        response.sendStatus(204);
+    }
+    catch (err) {
+        response.status(500).send(err.message);
+    }
+});
+
 module.exports = router;
